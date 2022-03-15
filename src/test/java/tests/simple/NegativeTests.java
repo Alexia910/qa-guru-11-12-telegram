ebf83852b87de22d0bde39663ab9eeb34d6dff48
Alexia910 <lesya.91099@gmail.com>
2022-03-15 21:34:52 +0300

    Added project

---
+++ src/test/java/tests/simple/NegativeTests.java
@@ -0,0 +1,18 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+    @Test
+    void test00() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test03() {
+        assertTrue(false);
+    }
+
+}
